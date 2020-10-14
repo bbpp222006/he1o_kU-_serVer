022992b320cbaab6e4fbf1a5beda3b5e26a2b48d
bbpp222006 <34976166+bbpp222006@users.noreply.github.com>
2020-10-14 18:12:15 +0800

    init

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 # main.py
-from fastapi import FastAPI
-app = FastAPI()
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
+from typing import Optional
+import pixiv_class
 
-@app.get("/")
-def hello():
-    return {"message":"Hello TutLinks.com"}
+
+app = FastAPI()
+pixiv=pixiv_class.Pixiv()
+
+@app.get("/pixiv")
+def hello(type: Optional[str] = Query("rank", regex="^(illust)|(rank)|(search)$"),
+    action: Optional[str] = Query(None, regex="^update$"),
+    id:Optional[int] = None,
+    rank_mode: Optional[str] = Query("week", regex="^(day)|(week)|(month)|(day_male)|(day_female)|(week_original)|(week_rookie)|(day_manga','day_r18)|(day_male_r18)|(day_female_r18)|(week_r18)|(week_r18g)$"),
+    rank_page: Optional[int] = 1,
+    rank_date: Optional[str] = Query(None, regex="^\d{4}-\d{2}-\d{2}$"),
+    search_word: Optional[str] = None,
+    search_mode: Optional[str] = Query("partial_match_for_tags", regex="^(partial_match_for_tags)|(exact_match_for_tags)|(title_and_caption)$"),
+    search_order: Optional[str] = Query("date_desc", regex="^(date_desc)|(date_asc)$"),
+    search_page: Optional[int] = 1):
+    if action=="update":
+        return_message =pixiv.api_init()
+    elif type=="illust":
+        return_message=pixiv.get_with_id(id)
+    elif type=="search":
+        return_message = pixiv.search_with_word(search_word,search_mode,search_order,search_page)
+    elif type=="rank":
+        return_message = pixiv.get_rank(rank_mode,rank_page,rank_date)
+    return {"message":return_message}
+
+# if __name__ == '__main__':
+#     import uvicorn
+#     uvicorn.run(app=app,host="0.0.0.0",port=25621,workers=1)
---
+++ pixiv_class.py
@@ -0,0 +1,34 @@
+from pixivpy3 import *
+import os
+
+class Pixiv():
+    def __init__(self):
+        self.api_init()
+
+    def api_init(self):
+        # self.pixiv_api = ByPassSniApi()  # Same as AppPixivAPI, but bypass the GFW
+        # self.pixiv_api.require_appapi_hosts()
+        # self.pixiv_api.set_accept_language('en-us')
+        self.pixiv_api = AppPixivAPI()  # Same as AppPixivAPI, but bypass the GFW
+        self.pixiv_api.login(os.environ.get('PIXUSER'), os.environ.get('PIXPASS'))
+        self.refresh_pixiv_list()
+        return "成功更新当日日榜"
+
+    def refresh_pixiv_list(self,date=None,offset = None):
+        modes = ['day', 'week', 'month', 'day_male', 'day_female', 'week_original', 'week_rookie', 'day_manga',
+                 'day_r18', 'day_male_r18', 'day_female_r18', 'week_r18', 'week_r18g']
+        self.pixiv_dic_list = {}
+        for mode in modes:
+            self.pixiv_dic_list[mode] = self.pixiv_api.illust_ranking(mode=mode, date=date, offset=offset)
+        return "成功更新为"+str(date)+"  "+str(offset)+"页"
+
+    def get_with_id(self,illust_id):
+        return self.pixiv_api.illust_detail(illust_id)
+
+    def search_with_word(self,word, search_target='partial_match_for_tags', sort='date_desc',offset=None):
+        return self.pixiv_api.search_illust(word=word, search_target=search_target, sort=sort, duration=None,offset=offset)
+
+    def get_rank(self,mode="day_male",page=1,date=None):
+        if page!=1 and date!=None:
+            self.refresh_pixiv_list(page,date)
+        return self.pixiv_dic_list[mode]
